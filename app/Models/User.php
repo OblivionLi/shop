9536188ea003ea724025a1eaf876d68dd0fa0bd9
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-03-08 15:15:39 +0200

    Add passport use call and roles relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -40,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles() 
+    {
+        return $this->belongsToMany(Role::class, 'user_role');
+    }
+
+    public function scopeInfo($query)
+    {
+        return $query->with('roles.permissions');
+    }
 }

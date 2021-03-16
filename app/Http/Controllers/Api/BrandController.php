2edbf79242413837458f8d885f065f1788474e98
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-03-16 15:09:30 +0200

    Add brand model and controller with it's CRUD functions

---
+++ app/Http/Controllers/Api/BrandController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBrandRequest;
+use App\Http\Requests\UpdateBrandRequest;
+use App\Http\Resources\BrandResource;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $brands = Brand::all();
+
+        return BrandResource::collection($brands);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBrandRequest $request)
+    {
+        $brand = new Brand();
+
+        $brand->brand_name = $request->brand_name;
+        $brand->brand_quantity = 0;
+
+        $brand->save();
+
+        return response()->json(['success' => 'Brand created successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $brand = Brand::find($id);
+
+        return response()->json($brand);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBrandRequest $request, $id)
+    {
+        $brand = Brand::find($id);
+
+        $brand->brand_name = $request->brand_name;
+        $brand->brand_quantity = $request->brand_quantity;
+
+        $brand->save();
+
+        return response()->json(['success' => 'Brand updated successfully']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $brand = Brand::find($id);
+        
+        $brand->products()->delete();
+        $brand->delete();
+
+        return response()->json(['success' => 'Brand deleted successfully']);
+    }
+}

05d1d0bd636e8dc433c3b58a147e31bdef7c6af5
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-03-22 15:38:25 +0200

    Add child cat. screen that display the data into a table

---
+++ resources/js/screens/admin/childCats/ChildCatScreen.js
@@ -0,0 +1,252 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Paper, Dialog, DialogActions, Link, Button } from "@material-ui/core";
+import MaterialTable from "material-table";
+import Loader from "../../../components/Loader";
+import Message from "../../../components/Message";
+import Swal from "sweetalert2";
+import AddChildCatScreen from "./AddChildCatScreen";
+import EditChildCatScreen from "./EditChildCatScreen";
+import Moment from "react-moment";
+import { adminListChildCats, deleteChildCat } from "../../../actions/childCatActions";
+
+const ChildCatScreen = ({ history }) => {
+    const dispatch = useDispatch();
+    const user_perms = [];
+
+    const [openAddDialog, setOpenAddDialog] = useState(false);
+    const [openEditDialog, setOpenEditDialog] = useState(false);
+    const [requestData, setRequestData] = useState(new Date());
+
+    const childCatAdminList = useSelector((state) => state.childCatAdminList);
+    const { loading, error, childCats } = childCatAdminList;
+
+    const childCatDelete = useSelector((state) => state.childCatDelete);
+    const {
+        loading: loadingDelete,
+        success: successDelete,
+        error: errorDelete,
+    } = childCatDelete;
+
+    console.log(childCats)
+
+    const userLogin = useSelector((state) => state.userLogin);
+    const { userInfo } = userLogin;
+
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [childCatId, setChildCatId] = useState(null);
+
+    useEffect(() => {
+        if (!userInfo || userInfo === null || userInfo.role[0].is_admin < 1) {
+            history.push("/login");
+        } else {
+            if (!user_perms.includes("admin_view_childCats")) {
+                history.push("/admin");
+                Swal.fire(
+                    "Sorry!",
+                    `You don't have access to this page.`,
+                    "warning"
+                );
+            } else {
+                setIsAdmin(true);
+                dispatch(adminListChildCats());
+            }
+        }
+    }, [dispatch, history, userInfo, requestData]);
+
+    if (!Array.isArray(user_perms) || !user_perms.length) {
+        userInfo.details[0].roles[0].permissions.map((perm) =>
+            user_perms.push(perm.name)
+        );
+    }
+
+    // handle add dialog opening
+    const handleAddDialogOpen = (e) => {
+        // open dialog
+        if (user_perms.includes("admin_add_childCats")) {
+            setOpenAddDialog(true);
+        } else {
+            Swal.fire(
+                "Sorry!",
+                `You don't have access to this action.`,
+                "warning"
+            );
+        }
+    };
+
+    // handle add dialog closing
+    const handleAddDialogClose = (e) => {
+        // close dialog
+        setOpenAddDialog(false);
+    };
+
+    // handle edit dialog opening
+    const handleEditDialogOpen = (id) => {
+        // open dialog
+        if (user_perms.includes("admin_edit_childCats")) {
+            setOpenEditDialog(true);
+            setChildCatId(id);
+        } else {
+            Swal.fire(
+                "Sorry!",
+                `You don't have access to this action.`,
+                "warning"
+            );
+        }
+    };
+
+    // handle edit dialog closing
+    const handleEditDialogClose = (e) => {
+        // close dialog
+        setOpenEditDialog(false);
+    };
+
+    const deleteChildCatHandler = (id) => {
+        user_perms.includes("admin_delete_childCats")
+            ? Swal.fire({
+                  title: "Are you sure?",
+                  text: "You can't recover this child category after deletion!",
+                  icon: "warning",
+                  showCancelButton: true,
+                  confirmButtonText: "Yes, delete it!",
+                  cancelButtonText: "No, cancel!",
+                  cancelButtonColor: "#d33",
+                  reverseButtons: true,
+              }).then((result) => {
+                  if (result.value) {
+                      dispatch(deleteChildCat(id));
+                      setRequestData(new Date());
+                      Swal.fire(
+                          "Deleted!",
+                          "The child category with the id " + id + " has been deleted.",
+                          "success"
+                      );
+                  } else if (result.dismiss === Swal.DismissReason.cancel) {
+                      Swal.fire(
+                          "Cancelled",
+                          `The selected child category is safe, don't worry :)`,
+                          "error"
+                      );
+                  }
+              })
+            : Swal.fire(
+                  "Sorry!",
+                  `You don't have access to this action.`,
+                  "warning"
+              );
+    };
+
+    return (
+        <Paper className="paper">
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant="error">{error}</Message>
+            ) : (
+                <MaterialTable
+                    title="Child Category List"
+                    columns={[
+                        {
+                            title: "Name",
+                            field: "child_category_name",
+                        },
+                        {
+                            title: "Quantity",
+                            field: "child_category_quantity",
+                        },
+                        {
+                            title: "Updated At",
+                            field: "updated_at",
+                            render: (childCats) => {
+                                {
+                                    return (
+                                        <Moment format="DD/MM/YYYY HH:mm">
+                                            {childCats.updated_at}
+                                        </Moment>
+                                    );
+                                }
+                            },
+                        },
+                    ]}
+                    data={childCats.data}
+                    actions={[
+                        {
+                            icon: "add",
+                            tooltip: "Add Child Category",
+                            isFreeAction: true,
+                            onClick: (event) => {
+                                handleAddDialogOpen(event);
+                            },
+                        },
+                        {
+                            icon: "edit",
+                            tooltip: "Edit Child Category",
+                            onClick: (event, rowData) => {
+                                handleEditDialogOpen(rowData.id);
+                            },
+                        },
+                        {
+                            icon: "delete",
+                            tooltip: "Delete Child Category",
+                            onClick: (event, rowData) => {
+                                deleteChildCatHandler(rowData.id);
+                            },
+                        },
+                    ]}
+                    options={{
+                        actionsColumnIndex: -1,
+                    }}
+                />
+            )}
+
+            <Dialog
+                open={openAddDialog}
+                aria-labelledby="draggable-dialog-title"
+                onClose={handleAddDialogClose}
+                fullWidth
+            >
+                <AddChildCatScreen
+                    setOpenAddDialog={setOpenAddDialog}
+                    setRequestData={setRequestData}
+                />
+
+                <DialogActions>
+                    <Button
+                        onClick={handleAddDialogClose}
+                        variant="contained"
+                        color="secondary"
+                        className="dialog-button"
+                    >
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <Dialog
+                open={openEditDialog}
+                aria-labelledby="draggable-dialog-title"
+                onClose={handleEditDialogClose}
+                fullWidth
+            >
+                <EditChildCatScreen
+                    setOpenEditDialog={setOpenEditDialog}
+                    setRequestData={setRequestData}
+                    childCatId={childCatId}
+                />
+
+                <DialogActions>
+                    <Button
+                        onClick={handleEditDialogClose}
+                        variant="contained"
+                        color="secondary"
+                        className="dialog-button"
+                    >
+                        Cancel
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </Paper>
+    )
+}
+
+export default ChildCatScreen

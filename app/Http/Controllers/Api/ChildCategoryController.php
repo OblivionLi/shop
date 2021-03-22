5b87ef964f675256ccbeb6d8d27eea01c7ee97b5
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-03-22 15:34:29 +0200

    Add model and controller for child categories

---
+++ app/Http/Controllers/Api/ChildCategoryController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreChildCategoryRequest;
+use App\Http\Requests\UpdateChildCategoryRequest;
+use App\Http\Resources\ChildCategoryResource;
+use App\Models\ChildCategory;
+use Illuminate\Http\Request;
+
+class ChildCategoryController extends Controller
+{
+       /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $child_categories = ChildCategory::all();
+
+        return ChildCategoryResource::collection($child_categories);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreChildCategoryRequest $request)
+    {
+        $child_category = new ChildCategory();
+
+        $child_category->child_category_name = $request->child_category_name;
+        $child_category->child_category_quantity = 0;
+
+        $child_category->save();
+
+        return response()->json(['success' => 'Child Category created successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $child_category = ChildCategory::find($id);
+
+        return response()->json($child_category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateChildCategoryRequest $request, $id)
+    {
+        $child_category = ChildCategory::find($id);
+
+        $child_category->child_category_name = $request->child_category_name;
+        $child_category->child_category_quantity = $request->child_category_quantity;
+
+        $child_category->save();
+
+        return response()->json(['success' => 'Child Category updated successfully']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $child_category = ChildCategory::find($id);
+        
+        $child_category->products()->sync([]);
+        $child_category->delete();
+
+        return response()->json(['success' => 'Child Category deleted successfully']);
+    }
+}
---
+++ app/Models/ChildCategory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ChildCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'child_category_name',
+        'child_category_quantity'
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    public function parentCats()
+    {
+        return $this->belongsToMany(ParentCategory::class, 'child_category_parent_category');
+    }
+}

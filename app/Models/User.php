5ae3a5db58268f2e946519c2c61ffc62098335e6
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-03-25 13:35:42 +0200

    Add user product relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,11 @@ class User extends Authenticatable
         return $this->belongsToMany(Role::class, 'user_role');
     }
 
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
     public function scopeInfo($query)
     {
         return $query->with('roles.permissions');

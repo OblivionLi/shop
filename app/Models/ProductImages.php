6a30cf9d8d1796d24db900c2b70e9822e7be7039
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-04-13 14:17:59 +0300

    Add product images model with relation to products model

---
+++ app/Models/ProductImages.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImages extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'path',
+        'product_id'
+    ];
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}

557fc5c295c360ab1407646c873bc3edbc4dabe1
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-04-13 14:17:34 +0300

    Add product model with its relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,6 +10,8 @@ class Product extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
+        'brand_id',
         'name',
         'product_code',
         'price',
@@ -21,4 +23,39 @@ class Product extends Model
         'type',
         'total_quantities'
     ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function sizes()
+    {
+        return $this->belongsToMany(Size::class, 'size_product')->withPivot(['size_quantity']);
+    }
+
+    public function colors()
+    {
+        return $this->belongsToMany(Color::class, 'color_product')->withPivot(['color_quantity']);
+    }
+
+    public function childCategories()
+    {
+        return $this->belongsToMany(ChildCategory::class, 'child_category_product');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function images() 
+    {
+        return $this->hasMany(ProductImages::class);
+    } 
+
+    public function scopeInfo($query)
+    {
+        return $query->with('brand', 'sizes', 'colors', 'childCategories', 'user', 'images');
+    }
 }

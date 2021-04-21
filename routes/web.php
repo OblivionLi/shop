f8f3403cfaf6e6e146e3492b29453c9953cf096d
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-04-21 17:41:12 +0300

    Add web route pattern that fixes 404 errors when accessing react routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layout.app');
+    return view('layout/app');
+});
+
+Route::pattern('path', '[a-zA-Z0-9-/]+');
+Route::any('{path}', function($page) {
+    return view('layout/app');
 });

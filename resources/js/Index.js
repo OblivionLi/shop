a395df93cab04cab63f24113b467d4bf105f5d9a
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-04-21 17:37:25 +0300

    Add auth react routes and react-redux store

--- resources/js/Index.js
+++ resources/js/Index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import store from "./store";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/public/users/LoginScreen';
+import RegisterScreen from './screens/public/users/RegisterScreen';
+import ResetPasswordScreen from './screens/public/users/ResetPasswordScreen';
+import ForgotPasswordScreen from './screens/public/users/ForgotPasswordScreen';
 
 function Index() {
     return (
         <Router>
             <Switch>
-                <Route path="/" component={HomeScreen} />
+                <Route path="/" component={HomeScreen} exact />
+                <Route path="/login" component={LoginScreen} />
+                <Route path="/register" component={RegisterScreen} />
+                <Route path="/reset-password/:id" component={ResetPasswordScreen} />
+                <Route path="/forgot-password" component={ForgotPasswordScreen} />
             </Switch>
         </Router>
     );
@@ -15,6 +25,11 @@ function Index() {
 
 export default Index;
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<Index />, document.getElementById('app'));
+if (document.getElementById("app")) {
+    ReactDOM.render(
+        <Provider store={store}>
+            <Index />
+        </Provider>,
+        document.getElementById("app")
+    );
 }

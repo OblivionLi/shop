5603fe1cb4258c53308a6be2ae6483edcae08ca6
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-05-17 16:08:57 +0300

    Add type controller that handles the CRUD operations

---
+++ app/Http/Controllers/Api/TypeController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TypeResource;
+use App\Models\Type;
+use Illuminate\Http\Request;
+
+class TypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $types = Type::with('parentCats')->get();
+
+        return TypeResource::collection($types);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $type = new Type();
+
+        $type->name = $request->name;
+
+        $type->save();
+
+        return response()->json(['success' => 'Type created successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $type = Type::find($id);
+
+        return response()->json($type);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $type = Type::find($id);
+
+        $type->name = $request->name;
+
+        $type->save();
+
+        return response()->json(['success' => 'Type updated successfully']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $type = Type::find($id);
+        
+        $type->parentCats()->delete();
+        $type->delete();
+
+        return response()->json(['success' => 'Type deleted successfully']);
+    }
+}

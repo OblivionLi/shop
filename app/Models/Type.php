52ca6aa5c3a3fd8f629820290bc07a8de108e8f2
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-05-17 16:07:52 +0300

    Add Type model class

---
+++ app/Models/Type.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Type extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'id', 'name'
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    public function parentCats()
+    {
+        return $this->belongsToMany(ParentCategory::class, 'type_parent_category');
+    }
+}

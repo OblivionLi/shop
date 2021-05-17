232407bfd19a9b07636aaeb302a6e48f64a69276
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-05-17 16:12:39 +0300

    Add relationships to parent category model class

--- app/Models/ParentCategory.php
+++ app/Models/ParentCategory.php
@@ -13,8 +13,23 @@ class ParentCategory extends Model
         'parent_category_name',
     ];
 
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
     public function childCats()
     {
         return $this->belongsToMany(ChildCategory::class, 'child_category_parent_category');
+    }
+
+    public function types()
+    {
+        return $this->belongsToMany(Type::class, 'type_parent_category');
+    }
+
+    public function scopeInfo($query)
+    {
+        return $query->with('childCats', 'types', 'products');
     }
 }

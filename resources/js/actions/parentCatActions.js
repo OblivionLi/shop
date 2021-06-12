7a7f6a167e4f7a70150fd14ba8dc7f9cada2ebc8
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-06-12 13:01:09 +0300

    Add function and params to parentCatActions.js react-redux file 	- add the listParentCats() function that calls the API route which will return a list of all parent categories (the function is intended to be accessed by non-logged-in users) 	- add params to createParentCat() and editParentCat() functions

--- resources/js/actions/parentCatActions.js
+++ resources/js/actions/parentCatActions.js
@@ -15,6 +15,9 @@ import {
     PARENT_CATEGORY_GET_DETAILS_FAIL,
     PARENT_CATEGORY_GET_DETAILS_REQUEST,
     PARENT_CATEGORY_GET_DETAILS_SUCCESS,
+    PARENT_CATEGORY_LIST_FAIL,
+    PARENT_CATEGORY_LIST_REQUEST,
+    PARENT_CATEGORY_LIST_SUCCESS,
 } from "../constants/parentCatConstants";
 
 export const adminListParentCats = () => async (dispatch, getState) => {
@@ -45,7 +48,25 @@ export const adminListParentCats = () => async (dispatch, getState) => {
     }
 };
 
-export const createParentCat = (parent_category_name) => async (dispatch, getState) => {
+export const listParentCats = () => async (dispatch, getState) => {
+    try {
+        dispatch({ type: PARENT_CATEGORY_LIST_REQUEST });
+
+        const { data } = await Axios.get(`/api/listParentCat`);
+
+        dispatch({ type: PARENT_CATEGORY_LIST_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({
+            type: PARENT_CATEGORY_LIST_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
+
+export const createParentCat = (parent_category_name, type) => async (dispatch, getState) => {
     try {
         dispatch({ type: PARENT_CATEGORY_CREATE_REQUEST });
 
@@ -59,7 +80,7 @@ export const createParentCat = (parent_category_name) => async (dispatch, getSta
             },
         };
 
-        const { data } = await Axios.post(`/api/parentCat`, { parent_category_name }, config);
+        const { data } = await Axios.post(`/api/parentCat`, { parent_category_name, type }, config);
 
         dispatch({ type: PARENT_CATEGORY_CREATE_SUCCESS, payload: data });
     } catch (error) {
@@ -101,7 +122,7 @@ export const getEditParentCatDetails = (id) => async (dispatch, getState) => {
     }
 };
 
-export const editParentCat = (parentId, parent_category_name) => async (
+export const editParentCat = (parentId, parent_category_name, type) => async (
     dispatch,
     getState
 ) => {
@@ -121,7 +142,7 @@ export const editParentCat = (parentId, parent_category_name) => async (
         const { data } = await Axios.patch(
             `/api/parentCat/${parentId}`,
             {
-                parent_category_name
+                parent_category_name, type
             },
             config
         );

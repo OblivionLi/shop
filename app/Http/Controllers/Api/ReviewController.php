8c8cdd67c2738ad7399f358076958bf3282f1334
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-06-12 12:40:36 +0300

    ReviewController format 	- fixed save() function condition, before fixing it checked if a user made a review by looking at the reviews table but only at the user_id, that gave an error, 		if the user made a review for a product, the condition applied it to all products, so the user wasn't allowed to make other reviews to other products because 		of it. (save() function allows only 1 review per user)
    	- fixed save() saving columns and saving the avg() rating (wasn't looking where it was intended
    	- added better error/success messages to save() function
    
    	- added CRUD functions for general use of reviews

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Review;
+use App\Models\User;
 use Error;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -19,13 +20,18 @@ class ReviewController extends Controller
         $product = Product::find($product_id);
 
         if ($product) {
-            $existingReview = Review::where('user_id', $loggedUser)->get();
+            $existingReview = Review::where([
+                ['product_id', '=', $product_id],
+                ['user_id', '=', $loggedUser]
+            ])->get();
+
             if ($existingReview->count() < 1) {
                 $review = new Review();
 
                 $review->product_id = $product_id;
-                $review->user_id = $user_id;
-                $review->name = $request->user_name;
+                // $review->user_id = $user_id->id;
+                $review->user_id = $loggedUser;
+                $review->name = $request->username;
                 $review->rating = $request->rating;
                 $review->comment = $request->comment;
         
@@ -34,18 +40,93 @@ class ReviewController extends Controller
                 $reviews = Review::where('product_id', $product_id)->get();
 
                 $product->total_reviews = $reviews->count();
-                $product->rating = $product->reviews->avg('rating');
+                $product->rating = $reviews->avg('rating');
 
                 $product->save();
             } else {
-                throw new Error('Product already reviewed', 1);
+                throw new Error('You reviewed this product already. Only one review per customer is allowed!', 1);
             }
         }
 
         $response = [
-            'message' => 'Review created successfully'
+            'message' => 'Review created successfully.'
         ];
 
         return response()->json($response, 200);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $reviews = Review::with('products', 'user')->get();
+
+        return response()->json($reviews);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function reviewsPag($product_id)
+    {
+        $reviews = Review::with('products')->where('product_id', '=', $product_id)->paginate(8);
+
+        return response()->json($reviews);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $review = Review::find($id);
+
+        return response()->json($review);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $review = Review::find($id);
+
+        $user_id = Auth::id();
+        $user = User::find($user_id);
+
+        $review->admin_name = $user->name;
+        $review->comment = $request->comment;
+        $review->admin_comment = $request->admin_comment;
+
+        $review->save();
+
+        return response()->json(['success' => 'Review updated successfully']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $review = Review::find($id);
+
+        $review->delete();
+
+        return response()->json(['success' => 'Review deleted succesfully!']);
     }
 }

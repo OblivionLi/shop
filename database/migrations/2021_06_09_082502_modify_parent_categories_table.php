abd574734d5869112e3e0632da55dc2f9e9f6e8b
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-06-12 12:24:10 +0300

    Reformat migrations

---
+++ database/migrations/2021_06_09_082502_modify_parent_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ModifyParentCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('parent_categories', function (Blueprint $table) {
+            $table->foreignId('type_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('parent_categories', function (Blueprint $table) {
+            //
+        });
+    }
+}

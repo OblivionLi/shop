80d0d089f444cbfaec466a8f40a192f19acbae11
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-07-12 13:20:06 +0300

    Add filtering and fix relationships
    	- Fix pivot relationships
    	- Add PRICES const message that will be displayed inside ProductsListScreen
    	- Add the scope func withFilters() that is called in all controllers which handles the filtering of the data based on the request inputs located in the "selected" object and also the query returns a products_count var that will display how many products are available for color "red" for example

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -31,27 +31,27 @@ class Product extends Model
 
     public function sizes()
     {
-        return $this->belongsToMany(Size::class, 'size_product')->withPivot(['size_quantity']);
+        return $this->belongsToMany(Size::class, 'size_product')->withPivot('size_id', 'size_quantity');
     }
 
     public function colors()
     {
-        return $this->belongsToMany(Color::class, 'color_product')->withPivot('id', 'color_quantity');
+        return $this->belongsToMany(Color::class, 'color_product')->withPivot('color_id', 'color_quantity');
     }
 
     public function childCategories()
     {
-        return $this->belongsToMany(ChildCategory::class, 'child_category_product');
+        return $this->belongsToMany(ChildCategory::class, 'child_category_product')->withPivot('product_id', 'child_category_id');
     }
 
     public function parentCategories()
     {
-        return $this->belongsToMany(ParentCategory::class, 'parent_category_product');
+        return $this->belongsToMany(ParentCategory::class, 'parent_category_product')->withPivot('product_id', 'parent_category_id');
     }
 
     public function types()
     {
-        return $this->belongsToMany(Type::class, 'type_product');
+        return $this->belongsToMany(Type::class, 'type_product')->withPivot('product_id', 'type_id');
     }
 
     public function user()
@@ -71,5 +71,45 @@ class Product extends Model
     public function scopeInfo($query)
     {
         return $query->with('brand', 'sizes', 'colors', 'childCategories', 'childCategories.parentCat', 'parentCategories', 'parentCategories.type', 'types', 'user', 'images', 'reviews');
+    }
+
+    const PRICES = [
+        'Less than 50',
+        'From 50 to 100',
+        'From 100 to 500',
+        'More than 500',
+    ];
+
+    public function scopeWithFilters($query, $prices, $brands, $sizes, $colors)
+    {
+        return $query->when(count($brands), function ($query) use ($brands) {
+                $query->whereIn('brand_id', $brands);
+            })
+            ->when(count($sizes), function ($query) use ($sizes) {
+                $query->whereHas('sizes', function ($q) use ($sizes) {
+                    $q->where('size_id', $sizes);
+                });
+            })
+            ->when(count($colors), function ($query) use ($colors) {
+                $query->whereHas('colors', function ($q) use ($colors) {
+                    $q->where('color_id', $colors);
+                });
+            })
+            ->when(count($prices), function ($query) use ($prices){
+                $query->where(function ($query) use ($prices) {
+                    $query->when(in_array(0, $prices), function ($query) {
+                            $query->orWhere('price', '<', '50.00');
+                        })
+                        ->when(in_array(1, $prices), function ($query) {
+                            $query->orWhereBetween('price', ['50.00', '100.00']);
+                        })
+                        ->when(in_array(2, $prices), function ($query) {
+                            $query->orWhereBetween('price', ['100.00', '500.00']);
+                        })
+                        ->when(in_array(3, $prices), function ($query) {
+                            $query->orWhere('price', '>', '500.00');
+                        });
+                });
+            });
     }
 }

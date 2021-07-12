522ef615d40388407c5c96eaec74bbc3ed0a944e
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-07-12 12:47:57 +0300

    Add const and reducers for react-redux
    	- the consts handles the get and edit actions of a user (not admin); allows changing logged in user settings

--- resources/js/reducers/userReducers.js
+++ resources/js/reducers/userReducers.js
@@ -5,6 +5,10 @@ import {
     USER_DELETE_FAIL,
     USER_DELETE_REQUEST,
     USER_DELETE_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_RESET,
+    USER_DETAILS_SUCCESS,
     USER_EDIT_FAIL,
     USER_EDIT_REQUEST,
     USER_EDIT_RESET,
@@ -28,6 +32,9 @@ import {
     USER_RESET_PASS_FAIL,
     USER_RESET_PASS_REQUEST,
     USER_RESET_PASS_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_SUCCESS,
 } from "../constants/userConstants";
 
 export const userRegisterReducer = (state = {}, action) => {
@@ -173,6 +180,42 @@ export const userEditReducer = (state = { user: {} }, action) => {
             return {
                 user: {},
             };
+        default:
+            return state;
+    }
+};
+
+export const userDetailsReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true };
+
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload };
+
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+
+        case USER_DETAILS_RESET:
+            return {
+                user: {}
+            };
+        default:
+            return state;
+    }
+};
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_UPDATE_PROFILE_REQUEST:
+            return { loading: true };
+
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, success: true, userInfo: action.payload };
+
+        case USER_UPDATE_PROFILE_FAIL:
+            return { loading: false, error: action.payload };
+
         default:
             return state;
     }
